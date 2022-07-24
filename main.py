1ce1193586212d11c35c979714e1d08f4fcee5ce
dhruv50ae <linkdp2012@gmail.com>
2022-07-24 12:24:25 +0100

    Add Error handling

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas
 
 data = pandas.read_csv("npa.csv")
 phoneticDict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phoneticDict)
-name = input("Enter a name: \n").upper()
-outputList = [phoneticDict[letter] for letter in name]
 
-print(outputList)
+def generatePhonetic():
+    name = input("Enter a name: \n").upper()
+    try:
+        outputList = [phoneticDict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generatePhonetic()
+    else:
+        print(outputList)
+
+generatePhonetic()
